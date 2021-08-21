a423a3728093cef5832dec3ee601a75e659d4ae4
Jason Knoll <78898641+JasonPKnoll@users.noreply.github.com>
2021-08-21 10:05:15 -0700

    Update variable name for total shape keys

--- apply_shapekeys.py
+++ apply_shapekeys.py
@@ -1,10 +1,10 @@
 import bpy
 
 # shape_key_index = 0 is Basis
-#totalShapeKeys = bpy.data.shape_keys.keys()
-#totalShapeKeys = bpy.context.object.data.shape_keys
+#total_shape_keys = bpy.data.shape_keys.keys()
+#total_shape_keys = bpy.context.object.data.shape_keys
 
 # total length of All shapekeys for Selected Item
-totalShapeKeys = len(bpy.context.object.data.shape_keys.key_blocks)
+total_shape_keys = len(bpy.context.object.data.shape_keys.key_blocks)
 
-print(totalShapeKeys)
+print(total_shape_keys)
