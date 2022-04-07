d1fb281e0374c89c28631ee8e4d8765cd79f5098
JasonPKnoll <Jason.p.knoll@gmail.com>
2022-04-06 20:42:44 -0700

    Duplicate objects and remove shapekeys from each

--- apply_shapekeys.py
+++ apply_shapekeys.py
@@ -1,3 +1,4 @@
+import pdb
 import bpy
 import json
 
@@ -9,14 +10,50 @@ import json
 totalShapeKeys = len(bpy.context.object.data.shape_keys.key_blocks)
 
 src_obj = bpy.context.active_object
+src_obj
+bpy.context.object.modifiers
 
+copied_objects = []
+# for shapekey in bpy.context.object.data.shape_keys.key_blocks:
+for shapekey in src_obj.data.shape_keys.key_blocks:
+    new_obj = bpy.context.active_object.copy()
+    new_data = bpy.context.active_object.data.copy()
+    new_obj.data = new_data
+    bpy.context.collection.objects.link(new_obj)
+
+    for new_shapekey in new_obj.data.shape_keys.key_blocks:
+        if new_shapekey.name != shapekey.name:
+            new_obj.shape_key_remove(new_shapekey)
+    copied_objects.append(new_obj)
+
+for i in range(len(copied_objects)):
+    print(i)
+    copied_objects[i].shape_key_clear()
+    # bpy.context.view_layer.objects.active = copied_objects[i]   
+    # bpy.context.object.modifier_apply(modifier="Mirror")
+
+    # copied_objects[i].modifier_apply(modifier="Mirror")
+    # if i != 0:
+    #     copied_objects[0].join_shapes(copied_objects[i])
+
+
+
+# for object in copied_objects:
+#     object.shape_key_clear()
+
+    
+#    new_obj = bpy.context.active_object.copy()
+#    new_data = bpy.context.active_object.data.copy()
+#    new_obj.data = new_data
+#    bpy.context.collection.objects.link(new_obj)
 #new_obj = src_obj.copy()
 #bpy.context.collection.objects.link(new_obj) #link object to scene
 #bpy.ops.object.make_single_user(object=True, obdata=True, material=False, animation=False) #unlink shapekeys of dup from main object
 
-new_obj = bpy.context.active_object.copy()
-new_data = bpy.context.active_object.data.copy()
-new_obj.data = new_data
-bpy.context.collection.objects.link(new_obj)
+
+# new_obj = bpy.context.active_object.copy()
+# new_data = bpy.context.active_object.data.copy()
+# new_obj.data = new_data
+# bpy.context.collection.objects.link(new_obj)
 
 print(totalShapeKeys)
