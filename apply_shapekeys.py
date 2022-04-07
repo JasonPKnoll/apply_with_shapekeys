3f2ad43f5214b88f9b735149c65e95b71ca31e65
JasonPKnoll <Jason.p.knoll@gmail.com>
2022-04-06 20:45:33 -0700

    Clean up old code

--- apply_shapekeys.py
+++ apply_shapekeys.py
@@ -1,10 +1,5 @@
 import pdb
 import bpy
-import json
-
-# shape_key_index = 0 is Basis
-#total_shape_keys = bpy.data.shape_keys.keys()
-#total_shape_keys = bpy.context.object.data.shape_keys
 
 # total length of All shapekeys for Selected Item
 totalShapeKeys = len(bpy.context.object.data.shape_keys.key_blocks)
@@ -35,25 +30,5 @@ for i in range(len(copied_objects)):
     # copied_objects[i].modifier_apply(modifier="Mirror")
     # if i != 0:
     #     copied_objects[0].join_shapes(copied_objects[i])
-
-
-
-# for object in copied_objects:
-#     object.shape_key_clear()
-
-    
-#    new_obj = bpy.context.active_object.copy()
-#    new_data = bpy.context.active_object.data.copy()
-#    new_obj.data = new_data
-#    bpy.context.collection.objects.link(new_obj)
-#new_obj = src_obj.copy()
-#bpy.context.collection.objects.link(new_obj) #link object to scene
-#bpy.ops.object.make_single_user(object=True, obdata=True, material=False, animation=False) #unlink shapekeys of dup from main object
-
-
-# new_obj = bpy.context.active_object.copy()
-# new_data = bpy.context.active_object.data.copy()
-# new_obj.data = new_data
-# bpy.context.collection.objects.link(new_obj)
 
 print(totalShapeKeys)
